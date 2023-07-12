eb6cf5622fa4852da478ce3e4beaefa128690289
Yash Guard <yashguard>
2023-07-12 11:03:26 +0530

    Only one time user can add

--- index.js
+++ index.js
@@ -4,24 +4,35 @@ server.use(express.json());
 let blankArray = [];
 
 server.get("/", (req, res) => {
-  res.status("200").send("Home page");
+  res.status(200).send("Home page is here");
 });
 
 server.get("/login", (req, res) => {
-  res.status("200").send("<h1>Login page</h1>");
+  res.status(200).send("<h1>Login page</h1>");
 });
 
 server.post("/signup", (req, res) => {
-  blankArray.push(req.body);
-  res.send("done");
+  let exist = false;
+  blankArray.map((ele, i) => {
+    if (ele.name == req.body.name) {
+      exist = true;
+    }
+  });
+  if (exist) {
+    res.send("User already exist");
+  } else {
+    blankArray.push(req.body)
+    console.log(blankArray);
+    res.send("User added");
+  }
 });
 
 server.get("/product", (req, res) => {
-  res.status("200").send("<h1>Product page</h1>");
+  res.status(200).send("<h1>Product page</h1>");
 });
 
 server.get("/json", (req, res) => {
-  res.status("200").send(blankArray);
+  res.status(200).send(blankArray);
 });
 
 server.listen(8010, () => {
